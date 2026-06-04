e5fae84b8361e1542f6261b9056fad67e89aa4e4
ramdani <xhijack@gmail.com>
2026-06-04 07:51:30 +0700

    feat(crm): Inbox CRM Settings single + child tables

---
+++ sopwer_inbox/sopwer_inbox/doctype/inbox_document_role/inbox_document_role.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, PT Sopwer Teknologi Indonesia and contributors
+# For license information, please see license.txt
+
+import frappe  # noqa: F401
+from frappe.model.document import Document
+
+
+class InboxDocumentRole(Document):
+	pass
